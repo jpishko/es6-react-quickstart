de8bb4e1b4502d06f210097798082dc924d58fbc
John Pishko <johnt921@yahoo.com>
2017-11-08 15:12:51 -0500

    Add files via upload

---
+++ src/js/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        React/ES6 Quickstart
+      </div>
+    );
+  }
+}
