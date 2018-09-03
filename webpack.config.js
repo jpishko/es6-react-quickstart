9e9da7c132213419401dcccf0e5a54cfc9a4f051
John Pishko <johnt921@yahoo.com>
2018-09-03 15:39:44 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,72 @@
 const webpack = require('webpack');
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const paths = {
-  DIST: path.resolve(__dirname, 'dist'),
-  SRC: path.resolve(__dirname, 'src'),
-  JS: path.resolve(__dirname, 'src/js'),
+    OUTPUT: path.resolve(__dirname, 'wwwroot'),
+    ENTRY: path.resolve(__dirname, 'ClientApp/js')
 };
 
 module.exports = {
-  entry: {
-    app: path.join(paths.JS, '/index.js')
-  },
+    entry: {
+        app: paths.ENTRY // points to app's entry point which is ./ClientApp/js/index.js
+    },
 
-  devtool: 'inline-source-map',
+    devtool: 'inline-source-map',
 
-  output: {
-    path: paths.DIST,
-    filename: 'bundle.js'
-  },
+    devServer: {
+        contentBase: paths.OUTPUT
+    },
 
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(paths.SRC, 'index.html'),
-    }),
-    new ExtractTextPlugin('bundle.css')
-  ],
+    output: {
+        path: paths.OUTPUT,
+        filename: 'bundle.js'
+    },
 
-  module: {
-    rules: [
-      // Transpile .js and .jsx files with Babel
-      // Babel configuration is located in ./babelrc
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
-        use: [ 
-          'babel-loader'
+    module: {
+        rules: [
+            // Transpile .js and .jsx files with Babel
+            // Babel configuration is located in ./babelrc
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules|bower_components)/,
+                use: ['babel-loader']
+            },
+
+            // Since webpack only understands JavaScript we have to use separate utilities called "loaders"
+            // to get our CSS into index.html.
+            // Step 1: Tell index.js about our CSS dependencies by importing the css files like this:
+            //  import './../css/style.css';
+            // Step 2: Use loaders to process .css, .scss files (*note* loaders listed in the 'use' array
+            //  run in reverse order)
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader', // creates style nodes from JS strings
+                    'css-loader' // translates CSS into CommonJS
+                ]
+            },
+
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader', // creates style nodes from JS strings
+                    'css-loader', // translates CSS into CommonJS
+                    'sass-loader' // compiles Sass to CSS, using Node Sass by default
+                ]
+            },
+
+            // File Loader handles loading image assets
+            // Other file types can be included as needed
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: ['file-loader']
+            },
+
+            {
+                test: /\.svg$/,
+                use: ['svg-inline-loader']
+            }
         ]
-      },
-
-      // Bundle CSS files
-      // Files will get handled by css loader and then passed to the extract text plugin
-      // which will bundle them into a single file whose name we defined above under plugins section
-      {
-        test: /\.css$/,
-        loader: ExtractTextPlugin.extract({
-          use: 'css-loader',
-        }),
-      },
-
-      // File Loader handles loading image assets
-      // Other file types can be included as needed
-      {
-        test: /\.(png|jpg|gif)$/,
-        use: [
-          'file-loader',
-        ],
-      }
-    ]
-  }
+    }
 
 };
