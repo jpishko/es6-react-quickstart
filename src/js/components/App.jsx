22ad573bd75d2ee3af14c48b7b690f1771828067
Pishko, John <16801@global.ul.com>
2018-11-21 15:24:46 -0500

    Initial implementation of react-router

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import {Route, NavLink, BrowserRouter} from 'react-router-dom';
+import Dashboard from './screens/Dashboard.jsx';
+import Screen1 from './screens/Screen1.jsx';
 
 export default class App extends React.Component {
   constructor() {
@@ -7,9 +10,19 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <div>
-        React/ES6 Quickstart
-      </div>
+      <BrowserRouter>
+        <div>
+            <h1>Simple SPA</h1>
+            <ul className="header">
+              <li><NavLink to="/">Dashboard</NavLink></li>
+              <li><NavLink to="/screen1">Screen1</NavLink></li>
+            </ul>
+            <div className="content">
+              <Route exact path="/" component={Dashboard}/>
+              <Route path="/screen1" component={Screen1}/>
+            </div>
+          </div>
+        </BrowserRouter>
     );
   }
 }
