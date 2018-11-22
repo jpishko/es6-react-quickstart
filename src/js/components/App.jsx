96e94127b380876ea5b51d0116fd267c3398355a
Pishko, John <16801@global.ul.com>
2018-11-22 06:33:58 -0500

    Added bootstrap and reactstrap dependencies

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Route, NavLink, BrowserRouter} from 'react-router-dom';
 import Dashboard from './screens/Dashboard.jsx';
 import Screen1 from './screens/Screen1.jsx';
+import { Button } from 'reactstrap';
 
 export default class App extends React.Component {
   constructor() {
@@ -13,6 +14,7 @@ export default class App extends React.Component {
       <BrowserRouter>
         <div>
             <h1>Simple SPA</h1>
+            <Button color="danger">Danger!</Button>
             <ul className="header">
               <li><NavLink to="/">Dashboard</NavLink></li>
               <li><NavLink to="/screen1">Screen1</NavLink></li>
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
-import '../css/reset.css';
+//import '../css/reset.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
