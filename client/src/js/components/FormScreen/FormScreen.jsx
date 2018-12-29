d3da858b5f94f600da234d60993c5ea77c3f67d2
Pishko, John <16801@global.ul.com>
2018-12-29 09:54:50 -0500

    Adjusted usage of BackBtnConsumer

--- client/src/js/components/FormScreen/FormScreen.jsx
+++ client/src/js/components/FormScreen/FormScreen.jsx
@@ -24,59 +24,59 @@ export default class FormScreen extends React.Component {
 
   render() {
     return (
-      <BackBtnConsumer>
-        {backBtnPath => (
-          <div>
-            <StyledMenuContainer>
-            <StyledMenu
-              onClick={this.handleMenuClick}
-              selectedKeys={[]}
-              mode="horizontal"
-              theme="dark"
-            >
-              <Menu.Item key="back">
-                <NavLink to={backBtnPath}><Icon type="caret-left" />Back</NavLink>
-              </Menu.Item>
-              <Menu.Item key="save">
-                <Icon type="save" />Save
-              </Menu.Item>
-              <SubMenu title={<span><Icon type="setting" />Actions</span>}>
-                <MenuItemGroup title="Item 1">
-                  <Menu.Item key="setting:1">Option 1</Menu.Item>
-                  <Menu.Item key="setting:2">Option 2</Menu.Item>
-                </MenuItemGroup>
-                <MenuItemGroup title="Item 2">
-                  <Menu.Item key="setting:3">Option 3</Menu.Item>
-                  <Menu.Item key="setting:4">Option 4</Menu.Item>
-                </MenuItemGroup>
-              </SubMenu>
-            </StyledMenu>
-          </StyledMenuContainer>
-  
-          <Spacer height="50"></Spacer>
-          <h1>Title</h1>
-  
-          <StyledTabContainer>
-            <Tabs onChange={this.handleTabChange} type="card">
-              <TabPane tab="Tab 1" key="1">Content of Tab Pane 1</TabPane>
-              <TabPane tab="Tab 2" key="2">Content of Tab Pane 2</TabPane>
-              <TabPane tab="Tab 3" key="3">Content of Tab Pane 3</TabPane>
-            </Tabs>
-          </StyledTabContainer>
-  
-          <Spacer height="25"></Spacer>
-  
-          <StyledCardContainer>
-            <Card
-              type="inner"
-              title="Section Title"
-            >
-              Content
-            </Card>
-          </StyledCardContainer>
-        </div>
-        )}
-      </BackBtnConsumer>
+      <div>
+        <StyledMenuContainer>
+          <StyledMenu
+            onClick={this.handleMenuClick}
+            selectedKeys={[]}
+            mode="horizontal"
+            theme="dark"
+          >
+            <Menu.Item key="back">
+              <BackBtnConsumer>
+                {backBtnPath => (
+                  <NavLink to={backBtnPath}><Icon type="caret-left" />Back</NavLink>
+                  )}
+              </BackBtnConsumer>
+            </Menu.Item>
+            <Menu.Item key="save">
+              <Icon type="save" />Save
+            </Menu.Item>
+            <SubMenu title={<span><Icon type="setting" />Actions</span>}>
+              <MenuItemGroup title="Item 1">
+                <Menu.Item key="setting:1">Option 1</Menu.Item>
+                <Menu.Item key="setting:2">Option 2</Menu.Item>
+              </MenuItemGroup>
+              <MenuItemGroup title="Item 2">
+                <Menu.Item key="setting:3">Option 3</Menu.Item>
+                <Menu.Item key="setting:4">Option 4</Menu.Item>
+              </MenuItemGroup>
+            </SubMenu>
+          </StyledMenu>
+        </StyledMenuContainer>
+
+        <Spacer height="50"></Spacer>
+        <h1>Title</h1>
+
+        <StyledTabContainer>
+          <Tabs onChange={this.handleTabChange} type="card">
+            <TabPane tab="Tab 1" key="1">Content of Tab Pane 1</TabPane>
+            <TabPane tab="Tab 2" key="2">Content of Tab Pane 2</TabPane>
+            <TabPane tab="Tab 3" key="3">Content of Tab Pane 3</TabPane>
+          </Tabs>
+        </StyledTabContainer>
+
+        <Spacer height="25"></Spacer>
+
+        <StyledCardContainer>
+          <Card
+            type="inner"
+            title="Section Title"
+          >
+            Content
+          </Card>
+        </StyledCardContainer>
+      </div>
     );
   }
 }
